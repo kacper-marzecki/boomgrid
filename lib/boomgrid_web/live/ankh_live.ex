d9c1fccc9c03c92e0f53054195f31c68acc36a97
Kacper Marzecki <kacper-marzecki@github.com>
2023-04-21 21:28:31 +0200

    fix

--- lib/boomgrid_web/live/ankh_live.ex
+++ lib/boomgrid_web/live/ankh_live.ex
@@ -5,8 +5,7 @@ defmodule BoomWeb.AnkhLive do
 
   def render(assigns) do
     ~H"""
-    <div id="debug_div">
-    <div>
+    <div id="debug_div"></div>
     <pre style="overflow: scroll; height: 200px;">
     <%= inspect(assigns, pretty: true) %>
     </pre>
